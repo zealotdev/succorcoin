f1056c8b3f5635f80091006501e0ccc2f0fe6cb2
zealotdev <g33ky.me@pm.me>
2022-02-05 17:25:39 +0300

    Refactor design

--- components/AboutUs/index.js
+++ components/AboutUs/index.js
@@ -20,31 +20,26 @@ export default function AboutUs() {
     tl.fromTo(
       aboutRef.current,
       { opacity: 0.1, y: 100 },
-      { opacity: 1, y: 0, duration: 1 }
+      { opacity: 1, y: -20, duration: 1 }
     );
   }, []);
 
   return (
-    <section className="about-us-bg bg-no-repeat bg-contain flex flex-col justify-center lg:px-24 xl:px-36">
-      <div ref={aboutRef}>
-        <h2 className="text-2xl font-bold text-purple-400 text-center mb-8">
-          About Us
-        </h2>
+    <section
+      ref={aboutRef}
+      className="about-us-bg bg-no-repeat bg-left bg-contain flex flex-col justify-center lg:px-24 xl:px-36"
+    >
+      <div>
         <div className="flex justify-evenly">
-          <div>
-            <Image
-              src="/logo.png"
-              height={400}
-              width={400}
-              alt="succorcoin-shield"
-            />
-          </div>
           <div className="flex flex-col space-y-4 w-6/12">
-            <span className="text-lg text-gray-500 underline underline-offset-1">
-              Revolutionary <i className="text-pink-400">Token</i> On{' '}
-              <i className="text-pink-400">BSC</i>
+            <h2 className="text-sm text-purple-400 tracking-wider uppercase">
+              About Us
+            </h2>
+            <span className="font-semibold text-2xl tracking-wide text-gray-700">
+              Revolutionary Token On{' '}
+              <span className="text-purple-400">BSC Network</span>
             </span>
-            <p className="text-gray-500">
+            <p className="text-gray-400 tracking-wider text-sm py-4">
               A renounced coin contract where the public have complete ownership
               and control of the contract, to assure longevity of the project
               through community ownership.
@@ -52,18 +47,26 @@ export default function AboutUs() {
 
             <div className="flex justify-between">
               <span className="rounded-full border-8 border-pink-400 flex flex-col justify-center items-center w-40 h-40 space-y-1">
-                <span className="font-extrabold text-2xl">75%</span>
+                <span className="font-extrabold text-xl">75%</span>
                 <p className="text-gray-500">BLOCKCHAIN</p>
               </span>
               <span className="rounded-full border-8 border-pink-400 flex flex-col justify-center items-center w-40 h-40 space-y-1">
-                <span className="font-extrabold text-2xl">80%</span>
+                <span className="font-extrabold text-xl">80%</span>
                 <p className="text-gray-500">COMMUNITY</p>
               </span>
               <span className="rounded-full border-8 border-pink-400 flex flex-col justify-center items-center w-40 h-40 space-y-1">
-                <span className="font-extrabold text-2xl">70%</span>
+                <span className="font-extrabold text-xl">70%</span>
                 <p className="text-gray-500">CHARITY</p>
               </span>
             </div>
+          </div>
+          <div>
+            <Image
+              src="/logo.png"
+              height={400}
+              width={400}
+              alt="succorcoin-shield"
+            />
           </div>
         </div>
       </div>
